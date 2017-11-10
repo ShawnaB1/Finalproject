6ecd29492556e1574f21ef24b032237fe03866c4
Shawna Beaudet <shawna.beaudet77@gmail.com>
2017-11-10 15:11:20 +0100

    tweak JS remove adtl dom readys

--- SBFinalproject.js
+++ SBFinalproject.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
  		this.src = 'images/routes/1Rondehoep.png';
 	});
 //change route map to image on #2 Tour de Gooi
-$(document).ready(function(){
 		document.getElementById("tourdegooiimg").addEventListener("mouseover", function(){
     	//alert("Hello World!");
  		this.src = 'images/tourdegooi680x360.png';
@@ -18,11 +17,8 @@ $(document).ready(function(){
 	document.getElementById("tourdegooiimg").addEventListener("mouseout", function(){
  		this.src = 'images/routes/tourdegooi.png';
 	});
-});
-
 
 //change route map to image on #3 Waterland
-$(document).ready(function(){
 	document.getElementById("waterlandimg").addEventListener("mouseover", function(){
  		this.src = 'images/waterland680x360.png';
 	});
@@ -36,13 +32,7 @@ $(document).ready(function(){
 	document.getElementById("beachdunesimg").addEventListener("mouseout", function(){
  		this.src = 'images/routes/dunesroute.png';
 	});
-});
-
-
-});
-
 //change route map to image on #6 Leimuiden
-$(document).ready(function(){
 	document.getElementById("leimuidensimg").addEventListener("mouseover", function(){
  		this.src = 'images/leimuiden.png';
 	});
